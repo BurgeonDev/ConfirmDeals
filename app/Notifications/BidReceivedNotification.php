ef5be9bc388b7b4c8b4b6fc4adba1e93aaa83a72
Najaf Ali <app@apps-MacBook-Pro.local>
2024-12-05 17:50:51 +0500

    “notifications”

--- app/Notifications/BidReceivedNotification.php
+++ app/Notifications/BidReceivedNotification.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\DatabaseMessage;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class BidReceivedNotification extends Notification
 {
@@ -16,7 +17,7 @@ class BidReceivedNotification extends Notification
 
     public function via($notifiable)
     {
-        return ['database']; // Use only the database channel
+        return ['database', 'mail']; // Use both database and mail channels
     }
 
     public function toDatabase($notifiable)
@@ -26,5 +27,14 @@ class BidReceivedNotification extends Notification
             'ad_id' => $this->ad->id,
             'user_id' => auth()->id()
         ]);
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('New Bid Received')
+            ->line('You have received a new bid on your ad: ' . $this->ad->title)
+            ->action('View Ad', url('/user/ad/' . $this->ad->id))
+            ->line('Thank you for using our platform!');
     }
 }
