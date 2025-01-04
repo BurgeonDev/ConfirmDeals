0bbf76bfaf1102a938ed6578fc6066d67fd39270
Najaf Ali <app@apps-MacBook-Pro.local>
2025-01-04 20:50:50 +0500

    “changes1”

--- database/migrations/2025_01_03_033610_add_created_by_and_updated_by_to_tables.php
+++ database/migrations/2025_01_03_033610_add_created_by_and_updated_by_to_tables.php
@@ -38,8 +38,12 @@ return new class extends Migration
         // Loop through each table and add the columns
         foreach ($tables as $table) {
             Schema::table($table, function (Blueprint $table) {
-                $table->string('created_by')->nullable();
-                $table->string('updated_by')->nullable();
+                $table->unsignedBigInteger('created_by')->nullable();
+                $table->unsignedBigInteger('updated_by')->nullable();
+
+                // Add the foreign key constraint
+                $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+                $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
             });
         }
     }
