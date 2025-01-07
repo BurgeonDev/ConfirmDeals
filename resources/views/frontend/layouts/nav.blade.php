7f06c23e9fd210ba5517ded8f7595ad8a3eb0e34
Najaf Ali <app@apps-MacBook-Pro.local>
2025-01-07 20:04:57 +0500

    “ServicesFix”

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -8,11 +8,6 @@
                          <a class="navbar-brand" href="{{ route('home') }}">
                              <img src="{{ asset('frontend/assets/images/logo/Logo1.svg') }}" alt="Logo">
                          </a>
-                         {{-- <a class="navbar-brand" href="{{ route('home') }}">
-                             <h2 style="display: inline; color: #582fe0;">Confirm</h2>
-                             <h3 style="display: inline;">Deals</h3>
-                         </a> --}}
-
                          <button class="navbar-toggler mobile-menu-btn" type="button" data-bs-toggle="collapse"
                              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                              aria-expanded="false" aria-label="Toggle navigation">
