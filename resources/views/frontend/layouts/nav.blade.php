d9a4a17f22806eca80725e2191148593bb43a55d
Najaf Ali <app@apps-MacBook-Pro.local>
2025-01-13 17:46:10 +0500

    “ServicesFix”

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -33,17 +33,7 @@
                                      <a href="{{ route('contact.index') }}" aria-label="Toggle navigation">Contact
                                          Us</a>
                                  </li>
-                                 {{-- <li class="nav-item">
-                                     <a class="dd-menu collapsed" href="javascript:void(0)" data-bs-toggle="collapse"
-                                         data-bs-target="#submenu-1-4" aria-controls="navbarSupportedContent"
-                                         aria-expanded="false" aria-label="Toggle navigation">Other</a>
-                                     <ul class="sub-menu mega-menu collapse" id="submenu-1-4">
-                                         <li class="nav-item"><a href="{{ route('home') }}">How It's Works</a></li>
-                                         <li class="nav-item"><a href="{{ route('faq') }}">FAQ</a></li>
-                                         <li class="nav-item"><a href="{{ route('pricing') }}">Pricing</a></li>
-                                         <li class="nav-item"><a href="{{ route('contact.index') }}">Contact Us</a></li>
-                                     </ul>
-                                 </li> --}}
+
                                  @can('Manage Admin Dashbaord')
                                      <li class="nav-item">
                                          <a href="{{ route('dashboard') }}" aria-label="Toggle navigation">Dashboard</a>
@@ -52,96 +42,7 @@
 
                              </ul>
                          </div>
-                         {{-- <div class="login-button">
 
-                             <ul>
-                                 @auth
-
-                                     <li>
-                                         <a>
-                                             <div class="notification-icon">
-                                                 <i class="lni lni-alarm" id="notificationIcon"></i>
-                                                 @if (auth()->check() && auth()->user()->unreadNotifications->count() > 0)
-                                                     <span class="notification-count"
-                                                         style="height: 15px; width: 15px; font-size: 10px; text-align: left;">
-                                                         {{ auth()->user()->unreadNotifications->count() }}
-                                                     </span>
-                                                     <div class="notification-dropdown" id="notificationDropdown"
-                                                         style="display: none;">
-                                                         <div class="notification-content">
-                                                             <button
-                                                                 style="margin-bottom: 10px; background-color: #282570; color: white; border: none; padding: 5px 10px; font-size: 0.6em; border-radius: 4px; cursor: pointer;"
-                                                                 onmouseover="this.style.backgroundColor='#0056b3';"
-                                                                 onmouseout="this.style.backgroundColor='#007bff';"
-                                                                 onclick="location.href='{{ route('notifications.markAllRead') }}'">
-                                                                 Mark All as Read
-                                                             </button>
-                                                             @foreach (auth()->user()->unreadNotifications as $notification)
-                                                                 <div style="margin-bottom: 10px;">
-                                                                     @if (isset($notification->data['url']))
-                                                                         <a href="{{ $notification->data['url'] }}">
-                                                                             {{ $notification->data['message'] }}
-                                                                         </a>
-                                                                     @else
-                                                                         {{ $notification->data['message'] }}
-                                                                     @endif
-                                                                     <span style="font-size: 0.9em; color: #888;">
-                                                                         {{ $notification->created_at->diffForHumans() }}
-                                                                     </span>
-                                                                 </div>
-                                                             @endforeach
-
-                                                         </div>
-                                                     </div>
-                                                 @else
-                                                     <div class="notification-dropdown" id="notificationDropdown"
-                                                         style="display: none;">
-                                                         <p>No new notifications</p>
-                                                     </div>
-                                                 @endif
-                                             </div>
-
-                                         </a>
-                                     </li>
-
-                                     <li>
-                                         <a href="{{ route('dashboard.index') }}">
-
-                                             <img style="max-height: 30px; max-width:35px; border-radius:50%; object-fit: cover;"
-                                                 src="{{ auth()->user()->profile_pic ? asset('storage/' . auth()->user()->profile_pic) : asset('frontend/assets/images/user/user.png') }}"
-                                                 alt="{{ auth()->user()->first_name }}" class="user-profile-img" />
-                                             {{ auth()->user()->first_name }}
-                                         </a>
-                                     </li>
-
-                                     <li>
-                                         <a href="{{ route('logout') }}"
-                                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                             <i class="lni lni-exit"></i> Logout
-                                         </a>
-                                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                             style="display: none;">
-                                             @csrf
-                                         </form>
-                                     </li>
-                                 @else
-                                     <li>
-                                         <a href="{{ route('login') }}"><i class="lni lni-enter"></i> Login</a>
-                                     </li>
-                                     <li>
-                                         <a href="{{ route('register') }}"><i class="lni lni-user"></i> Register</a>
-                                     </li>
-                                 @endauth
-                                 @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-                                     <li>
-                                         <a
-                                             href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
-                                             {{ $properties['native'] }}
-                                         </a>
-                                     </li>
-                                 @endforeach
-                             </ul>
-                         </div> --}}
                          <div class="login-button">
                              <ul>
                                  @auth
